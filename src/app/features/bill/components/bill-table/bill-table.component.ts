63966afe943f485502af3d5c8f3d4ecf152cf036
MoisesL27 <leonormoises27@outlook.com>
2020-04-09 02:30:07 -0600

    basic implementation of the table

--- src/app/features/bill/components/bill-table/bill-table.component.ts
+++ src/app/features/bill/components/bill-table/bill-table.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
-import { FormBuilder, FormControl, FormArray } from '@angular/forms';
-import { ControlDataTableConfig } from '@shared/types';
-import { BehaviorSubject, of, merge, Subject } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import { BehaviorSubject, merge, Subject } from 'rxjs';
+import { debounceTime, filter, map, pairwise, startWith } from 'rxjs/operators';
 import { BillTableConfiguration } from './bill-table-configuration';
-import { map, pairwise, startWith, filter, switchMap } from 'rxjs/operators';
 
 type Key = number;
 type Row = number;
@@ -26,9 +25,38 @@ export class BillTableComponent implements OnInit {
     { id: 3, value: 30, label: 'P2' },
   ];
   config = this.tableConfig.getConfiguration();
+
+  private updateTotal$ = new Subject<any>();
+
   constructor(private fb: FormBuilder, private tableConfig: BillTableConfiguration) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const quantity$ = this.updateTotal$.pipe(
+      startWith({ value: '', row: 0, column: 'quantity' }),
+      filter((result) => result.column === 'quantity'),
+      debounceTime(500),
+      pairwise(),
+      map(([prev, current]) => {
+        const reguex = /^[0-9]+$/;
+        const obj = this.getTotalObj(reguex, prev, current);
+        return this.updateTotal(obj);
+      })
+    );
+
+    const price$ = this.updateTotal$.pipe(
+      startWith({ value: '', row: 0, column: 'price' }),
+      filter((result) => result.column === 'price'),
+      debounceTime(500),
+      pairwise(),
+      map(([prev, current]) => {
+        const reguex = /^[0-9]+(\.[0-9]{1,4})?$/;
+        const obj = this.getTotalObj(reguex, prev, current);
+        return this.updateTotal(obj);
+      })
+    );
+
+    merge(quantity$, price$).subscribe((records) => this.source$.next(records));
+  }
 
   add() {
     const product = new FormControl();
@@ -53,7 +81,7 @@ export class BillTableComponent implements OnInit {
     return records;
   }
 
-  updateRecord(inventory: any, row: number) {
+  updateRecord(inventory: any, row: number, column: string) {
     const { id } = inventory;
 
     const rowHasProduct = this.selectedRows.has(row);
@@ -66,21 +94,61 @@ export class BillTableComponent implements OnInit {
 
     const records = this.source$.value.slice();
     const control = records[row].price as FormControl;
-    control.setValue(inventory.value);
+    const quantity = records[row].quantity as FormControl;
+
+    const price = inventory.value;
+
+    records[row].total = price * quantity.value;
+    control.setValue(price);
+
     return records;
   }
 
-  getInventory(inventory: any, row: number) {
+  updateDataSource(inventory: any, row: number, column: string) {
     const { id } = inventory;
     const rowHasProduct = this.selectedProducts.get(id) !== undefined;
 
     if (!rowHasProduct) {
-      const records = this.updateRecord(inventory, row);
+      const records = this.updateRecord(inventory, row, column);
       this.source$.next(records);
     } else {
       const records = this.cleanRecord(row);
       this.source$.next(records);
     }
+  }
+
+  computeTotal(event: KeyboardEvent, row: number, column: string) {
+    const input = event.target as HTMLInputElement;
+    const value = input.value.trim();
+
+    this.updateTotal$.next({ value, row, column });
+  }
+
+  private getTotalObj(reguex: RegExp, prev: any, current: any) {
+    const { value, row, column } = current;
+    return { value, row, reguex, prevValue: prev.value, column };
+  }
+
+  private updateTotal(obj: any) {
+    const { value, row, reguex, prevValue, column } = obj;
+    const records = this.source$.value.slice();
+    if (value === '') {
+      records[row].total = 0;
+    } else {
+      const price = records[row].price as FormControl;
+      if (reguex.test(value)) {
+        records[row].total = parseInt(value, 10) * price.value;
+      } else {
+        const val =
+          !reguex.test(prevValue) && (prevValue === '' || value.length === 1)
+            ? 0
+            : parseInt(prevValue, 10);
+        const control = records[row][column] as FormControl;
+        control.setValue(val);
+        records[row].total = val * price.value;
+      }
+    }
+    return records;
   }
 
   private clearCache(row: number) {
