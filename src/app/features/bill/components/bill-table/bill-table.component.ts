5433a8b73df26248da62f91c6f8b4eebbd5ed9b0
MoisesL27 <leonormoises27@outlook.com>
2020-04-10 00:22:03 -0600

    add comprare function to select in bill table

--- src/app/features/bill/components/bill-table/bill-table.component.ts
+++ src/app/features/bill/components/bill-table/bill-table.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
-import { BehaviorSubject, merge, Subject } from 'rxjs';
-import { debounceTime, filter, map, pairwise, startWith } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MessageService } from '@core/services/message.service';
+import { BehaviorSubject, merge, Observable, Subject } from 'rxjs';
+import { debounceTime, filter, map } from 'rxjs/operators';
 import { BillTableConfiguration } from './bill-table-configuration';
 import { TotalBillService } from './total.service';
 
@@ -20,34 +21,44 @@ export class BillTableComponent implements OnInit {
   private selectedRows = new Map<Row, Key>();
   private updateTotal$ = new Subject<any>();
 
+  options$: Observable<any[]>;
   records$ = new BehaviorSubject([]);
-  options = [
-    { id: 1, value: 20, label: 'P1' },
-    { id: 2, value: 10, label: 'P3' },
-    { id: 3, value: 30, label: 'P2' },
-  ];
+
+  @Input()
+  set products(products$: Observable<any[]>) {
+    this.options$ = products$;
+  }
+
   config = this.tableConfig.getConfiguration();
 
-  constructor(private total: TotalBillService, private tableConfig: BillTableConfiguration) {}
+  constructor(
+    private total: TotalBillService,
+    private tableConfig: BillTableConfiguration,
+    private message: MessageService
+  ) {}
 
   ngOnInit(): void {
     const quantity$ = this.getUpdateTotalObservable('quantity');
     const price$ = this.getUpdateTotalObservable('price');
-
     merge(quantity$, price$).subscribe((records) => this.records$.next(records));
   }
 
-  add() {
-    const product = new FormControl(Validators.required);
-    const quantity = new FormControl(0, [Validators.required, Validators.pattern(/^[0-9]+$/)]);
-    const price = new FormControl(0, [
-      Validators.required,
-      Validators.pattern(/^[0-9]+(\.[0-9]{1,4})?$/),
-    ]);
+  addRow() {
+    if (this.canAddMore()) {
+      const controls = this.getRowControls();
+      const newRow = { ...controls, total: 0 };
+      const rows = this.records$.value;
+      this.records$.next([...rows, newRow]);
+    } else {
+      this.message.error('');
+    }
+  }
 
-    const newRow = { product, quantity, price, total: 0 };
-    const rows = this.records$.value;
-    this.records$.next([...rows, newRow]);
+  deleteRow(row: number) {
+    const records = this.records$.value.slice();
+    const newRecords = records.filter((record, i) => i !== row);
+    this.removeCachedProduct(row);
+    this.records$.next(newRecords);
   }
 
   computeTotal(row: number, column: string) {
@@ -57,13 +68,11 @@ export class BillTableComponent implements OnInit {
   getSelectedProduct(product: any, row: number) {
     const { id } = product;
     const rowHasProduct = this.selectedProducts.get(id) !== undefined;
-
     if (!rowHasProduct) {
       const priceControl = this.records$.value[row].price as FormControl;
       priceControl.setValue(product.value);
 
-      this.storeProduct(product, row);
-
+      this.cachingProduct(product, row);
       this.computeTotal(row, 'price');
     } else {
       const records = this.unselectProduct(row);
@@ -71,6 +80,10 @@ export class BillTableComponent implements OnInit {
     }
   }
 
+  /**
+   * @description If the product was selected per other row in the table, the current
+   * selected product not will be selected for the user
+   */
   unselectProduct(row: number) {
     this.removeCachedProduct(row);
 
@@ -92,20 +105,28 @@ export class BillTableComponent implements OnInit {
         const records = this.records$.value.slice();
         const obj = { column: res.column, row: res.row, records };
         const total = this.total.getTotal(obj);
-        records[res.row].total = total;
+        records[res.row].total = total.toFixed(2);
         return records;
       })
     );
   }
 
-  private storeProduct(product: any, row: number) {
-    const { id } = product;
+  updatePrices() {
+    const records = this.records$.value;
+    console.log(records);
 
+    records.map((r) => {
+      console.log(r.product.value);
+      return r;
+    });
+  }
+
+  private cachingProduct(product: any, row: number) {
+    const { id } = product;
     const rowHasProduct = this.selectedRows.has(row);
     if (rowHasProduct) {
       this.removeCachedProduct(row);
     }
-
     this.selectedProducts.set(id, row);
     this.selectedRows.set(row, id);
   }
@@ -116,5 +137,30 @@ export class BillTableComponent implements OnInit {
 
     this.selectedProducts.delete(product);
     this.selectedRows.delete(rowToDelete);
+  }
+
+  private canAddMore() {
+    const records = this.records$.value;
+    if (records.length === 0) {
+      return true;
+    }
+    const last = records[records.length - 1];
+    return last.product.valid && last.quantity.valid && last.price.valid;
+  }
+
+  private getRowControls() {
+    const product = new FormControl(null, [Validators.required]);
+    const quantity = new FormControl(1, [Validators.required, Validators.pattern(/^[0-9]+$/)]);
+    const price = new FormControl(0, [
+      Validators.required,
+      Validators.pattern(/^[0-9]+(\.[0-9]{1,4})?$/),
+      Validators.min(0.1),
+    ]);
+
+    return { product, quantity, price };
+  }
+
+  compareFn(c1: any, c2: any): boolean {
+    return c1 && c2 ? c1.id === c2.id : c1 === c2;
   }
 }
